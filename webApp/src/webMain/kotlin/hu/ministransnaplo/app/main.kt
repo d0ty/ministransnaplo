ebd35e545664c40640922808ea6164bd3ab33b0c
d0ty <hp.raszpi@gmail.com>
2026-06-29 14:11:44 +0200

    added gpl license

--- webApp/src/webMain/kotlin/hu/ministransnaplo/app/main.kt
+++ webApp/src/webMain/kotlin/hu/ministransnaplo/app/main.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright 2026 doty and László Rab
+ * Use of this source code is governed by the GNU General Public License that can be found in the LICENSE file.
+ */
+
+
 package hu.ministransnaplo.app
 
 import androidx.compose.ui.ExperimentalComposeUiApi
