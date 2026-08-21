497f87800d7f93074cfa6056f51d5f5a3243cb18
d0ty <hp.raszpi@gmail.com>
2026-08-21 14:12:57 +0200

    member details: data card API cleanup

--- shared/src/commonMain/kotlin/hu/ministransnaplo/app/ui/components/DataCard.kt
+++ shared/src/commonMain/kotlin/hu/ministransnaplo/app/ui/components/DataCard.kt
@@ -3,6 +3,9 @@
  * Use of this source code is governed by the GNU General Public License that can be found at the LICENSE file
  */
 
+// Needed for DCFieldValue subclass casting
+@file:Suppress("UNCHECKED_CAST")
+
 package hu.ministransnaplo.app.ui.components
 
 import androidx.compose.foundation.Image
@@ -27,11 +30,36 @@ import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 import kotlinx.serialization.json.*
 
+/**
+ * Wrapper interface for Data Card Field values.
+ *
+ * **Supported types:**
+ *  - String: [DCFieldValue.Str]
+ *  - Boolean: [DCFieldValue.Bool]
+ *
+ *  Other types can be implemented by adding child data classes with the type you like.
+ */
+@Serializable(with = DCFieldValueSerializer::class)
+sealed interface DCFieldValue {
+    /**
+     * Data Card Field Value wrapper for type String.
+     * @see DCFieldValue
+     */
+    data class Str(val value: String) : DCFieldValue
 
+    /**
+     * Data Card Field Value wrapper for type Boolean.
+     * @see DCFieldValue
+     */
+    data class Bool(val value: Boolean) : DCFieldValue
+}
+
+/**
+ * Custom serializer for [DCFieldValue] to handle polymorphic serialization.
+ *
+ * **Works with JSON only!**
+ */
 object DCFieldValueSerializer : KSerializer<DCFieldValue> {
-
-    // The descriptor kind doesn't matter much here since we bypass it via JsonEncoder/JsonDecoder,
-    // but it must be declared. JsonElement-kind descriptors are the idiomatic choice.
     override val descriptor: SerialDescriptor =
         buildClassSerialDescriptor("DCFieldValue")
 
@@ -57,17 +85,26 @@ object DCFieldValueSerializer : KSerializer<DCFieldValue> {
     }
 }
 
-@Serializable(with = DCFieldValueSerializer::class)
-sealed interface DCFieldValue {
-    data class Str(val value: String) : DCFieldValue
-    data class Bool(val value: Boolean) : DCFieldValue
-}
-
+/**
+ * DSL Marker for [DataCard] API
+ */
 @DslMarker
 annotation class DataCardDsl
 
+/**
+ * Public API for registering data fields.
+ * @see DataCardScopeImpl
+ */
 @DataCardDsl
 interface DataCardScope {
+    /**
+     * Single line text data field.
+     *
+     * @param field - id of the text field. Used to manage edit state.
+     * @param title - visible label for the field
+     * @param value - the value displayed in static mode and the initial value of the input field
+     * @param readOnly - whenever the text field is meant to be edited or not
+     */
     @Composable
     fun TextField(
         field: String,
@@ -76,6 +113,13 @@ interface DataCardScope {
         readOnly: Boolean = false
     )
 
+    /**
+     * A simple checkbox data field.
+     *
+     * @param field - id of field for managing edit state
+     * @param title - visual label of the field
+     * @param checked - initial state of checkbox
+     */
     @Composable
     fun CheckboxField(
         field: String,
@@ -84,11 +128,31 @@ interface DataCardScope {
     )
 }
 
+/**
+ * Actual implementation of [DataCardScope] interface.
+ * Should never be used outside of [DataCard].
+ *
+ * @param editing indicated if we are in editing or static display mode
+ * @see DataCardScope
+ */
 class DataCardScopeImpl(editing: Boolean) : DataCardScope {
+    /**
+     * The actual state of fields.
+     * Key is a string that identifies the field. A value is a mutable state of some kind of [DCFieldValue].
+     */
     var fields = mutableStateMapOf<String, MutableState<DCFieldValue>>()
         private set
+
+    /**
+     * State wrapper of construction property editing.
+     */
     private var editing by mutableStateOf(editing)
 
+    /**
+     * Initiates a field state.
+     * @param field id of field
+     * @param value field value wrapped in a [DCFieldValue]
+     */
     private fun <T : DCFieldValue> stateFor(field: String, value: T): MutableState<DCFieldValue> =
         fields.getOrPut(field) { mutableStateOf(value) }
 
@@ -97,11 +161,14 @@ class DataCardScopeImpl(editing: Boolean) : DataCardScope {
         field: String, title: String,
         value: String, readOnly: Boolean
     ) {
+        // read only text fields are marked to be excluded from form results
         val fieldName = if (readOnly) "ro-$field" else field
         val textFieldValue: MutableState<DCFieldValue.Str> =
             stateFor(fieldName, DCFieldValue.Str(value)) as MutableState<DCFieldValue.Str>
 
         SideEffect {
+            // In read-only mode the external state is synced with the internal,
+            // while in editable fields, the internal state is synced with the form state
             if (readOnly) textFieldValue.value = DCFieldValue.Str(value)
             else fields[field]!!.value = textFieldValue.value
         }
@@ -148,17 +215,42 @@ class DataCardScopeImpl(editing: Boolean) : DataCardScope {
         }
     }
 
+    /**
+     * Syncs the internal [editing] state with [DataCard]'s external control signal.
+     * Should not be used outside of [DataCard]
+     *
+     * @param editing the editing state to be synced
+     */
     fun syncEditing(editing: Boolean) {
         this.editing = editing
     }
 
-    fun collectFormState() =
+    /**
+     * Retrieves the form state.
+     * Read only text fields are being excluded from results.
+     *
+     * Should only be used in [DataCard].
+     *
+     * @return The form state map cleaned up from [MutableState] wrappers and read-only text fields.
+     * Keys are field ids defined in field declarations and
+     * values are their current value wrapped into a [DCFieldValue] instance.
+     */
+    fun collectFormState(): Map<String, DCFieldValue> =
         fields
             .filter { it.key.contains("ro-").not() }
             .map { it.key to it.value.value }
             .toMap()
 }
 
+/**
+ * A data display card with the ability to edit the data inside.
+ *
+ * @param modifier Style using the [Modifier] Composer API
+ * @param editing Switch between editing and static view mode
+ * @param onEditFinishes on submit callback.
+ * If the edit is cancelled, no form state is passed.
+ * @param registerFields [DataCardScope] receivers to register your fields
+ */
 @Composable
 fun DataCard(
     modifier: Modifier = Modifier,
@@ -167,7 +259,7 @@ fun DataCard(
     registerFields: @Composable DataCardScope.() -> Unit = {},
 ) {
     val scope by remember { mutableStateOf(DataCardScopeImpl(editing)) }
-    SideEffect { scope.syncEditing(editing) }
+    SideEffect { scope.syncEditing(editing) } // syncs editing state with scpe
 
     CardColumn(modifier, Alignment.Start) {
         Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
